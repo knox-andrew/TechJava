712814485a16af91d3fa2f235e0e62b82e5746ea
David P-C <david.pc@techelevator.com>
2019-05-22 08:44:30 -0400

    Added student-lecture for module 1-8.

---
+++ module-1/08_Collections_Part_2/student-lecture/java/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,36 @@
+package com.techelevator;
+
+
+public class Lecture {
+
+	public static void main(String[] args) {
+
+		System.out.println("####################");
+		System.out.println("       MAPS");
+		System.out.println("####################");
+		System.out.println();
+
+		
+
+		System.out.println("####################");
+		System.out.println("       SETS");
+		System.out.println("####################");
+		System.out.println();
+
+
+		System.out.println("####################");
+		System.out.println("Sets cannot contain duplicates");
+		System.out.println("####################");
+		System.out.println();
+
+		
+		System.out.println("####################");
+		System.out.println("Sets do not guarantee ordering");
+		System.out.println("####################");
+		System.out.println();
+
+		
+
+	}
+
+}
